8ef108e4933e283f0553ee297677e97432458c43
Tethik <joakim@uddholm.com>
2018-05-08 01:05:52 +0200

    hack to avoid pdf being overwritten during checkout.

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,7 +2,9 @@
 
 git config --global user.email "circleci@blacknode.se"
 git config --global user.name "CircleCI Deployment"
+mv cv.pdf ..
 git checkout master
+mv ../cv.pdf .
 git add cv.pdf
 git commit -m "PDF build $CIRCLE_SHA1"
 git push origin master
