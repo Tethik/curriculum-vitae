e244ab30d4479d134e47e22a34d78d8ab9dba57a
Tethik <joakim@uddholm.com>
2018-05-20 23:21:03 +0200

    maybe committing the circleci files on master will stop the build?

---
+++ .circleci/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git config --global user.email "circleci@blacknode.se"
+git config --global user.name "CircleCI Deployment"
+mv cv.pdf ..
+git fetch --all
+git reset --hard origin/develop
+git checkout master
+mv ../cv.pdf .
+git add cv.pdf
+git commit -m "PDF build $CIRCLE_SHA1"
+git push origin master
